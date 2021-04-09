d2e84e3135eef4d2bc7c776f31185ba4b3f9739d
TrondEinar <trond-einar98@hotmail.no>
2021-04-09 15:38:49 +0200

    connect the recipe-API to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React, { useEffect, useState} from 'react'
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = "718a7f36";
+  const APP_KEY = "5e90b2093568757314ac8343c695fcfb"
+
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q='chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits)
+  }
+
+  return(
     <div className="App">
-    <h1>Hello World</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"></input>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
